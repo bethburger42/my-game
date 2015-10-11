857165597ca9031b50f4c9e5bfb1eeb39928c80b
Beth Ernsberger <bether.ga.2015@gmail.com>
2015-10-11 10:59:26 -0700

    added test for cards and empty hand

--- script.js
+++ script.js
@@ -55,36 +55,75 @@ var dealToPlayers = function() {
 	}
 }
 
+var testForWin = function() {
+	if(player1PlayedCards.length === 52) {
+		alert("Player 1 has all the cards. Player 1 WINS!!!");
+	} else if(player2PlayedCards.length === 52){
+		alert("Player 2 has all the cards. Player 2 WINS!!!");
+	}
+}
 
-// var testForWin = function() {
-// 	if(player1PlayedCards.length === 52) {
-// 		alert("Player 1 has all the cards. Player 1 WINS!!!");
-// 	} else if(player2PlayedCards.length === 52){
-// 		alert("Player 2 has all the cards. Player 2 WINS!!!");
-// 	}
+var emptyHand = function(activeArray, playedArray, play1, play2) {
+		if(playedArray.length > 0) {
+			activeArray = playedArray;
+			return true;
+		} else {
+			alert(play1 + " has no more cards to play. " + play2 + " wins!");
+			return false;
+		}
+}
+
+var testForCards = function (a1, a2, p1, p2) {
+	firstPlayer = "Player 1";
+	secondPlayer = "Player 2";
+	if((a1.length > 0) && (a2.length > 0)) {
+		return true;
+	}
+	else if(a1.length === 0) {
+		if(emptyHand(a1, p1, firstPlayer, secondPlayer)) {
+			return true;
+		} else {return false;}
+	} 
+	if(a2.length === 0) {
+		if(emptyHand(a2, p2, secondPlayer, firstPlayer)) {
+			return true;
+		} else {return false;}
+	} 
+	}
+}
+
+// var playWar = function() {
+// 	gamePlayArray[cardCounter] = player1ActiveCards.shift();
+// 	cardCounter++;
+// 	gamePlayArray[cardCounter] = player2ActiveCards.shift();
+// 	cardCounter++;
 // }
 
 var playHand = function() {
-	if(player1ActiveCards[0].rating > player2ActiveCards[0].rating) {
-		console.log("player 1 card " + player1ActiveCards[0].rating + " wins over: " + player2ActiveCards[0].rating);
-		player1PlayedCards[0] = player1ActiveCards.shift();
-		player1PlayedCards[1] = player2ActiveCards.shift();
-		console.log("player 1 array length: " + player1PlayedCards.length);
-		console.log("player 2 array length: " + player2PlayedCards.length);
-		// testForWin();
-	} else if(player1ActiveCards[0].rating < player2ActiveCards[0].rating) {
-		console.log("player 2 card " + player2ActiveCards[0].rating + " wins over: " + player1ActiveCards[0].rating);
-		player2PlayedCards[0] = player1ActiveCards.shift();
-		player2PlayedCards[1] = player2ActiveCards.shift();
-		console.log("player 1 array length: " + player1PlayedCards.length);
-		console.log("player 2 array length: " + player2PlayedCards.length);	
-		// testForWin();
-	} else if(player1ActiveCards[0].rating === player2ActiveCards[0].rating) {
+	var a1 = player1ActiveCards;
+	var a2 = player2ActiveCards;
+	var p1 = player1PlayedCards;
+	var p2 = player2PlayedCards;
+
+	if(a1[0].rating > a2[0].rating) {
+		console.log("player 1 card " + a1[0].rating + " wins over: " + a2[0].rating);
+		p1[0] = a1.shift();
+		p1[1] = a2.shift();
+		// console.log("player 1 array length: " + player1PlayedCards.length);
+		// console.log("player 2 array length: " + player2PlayedCards.length);
+		testForWin();
+	} else if(a1[0].rating < a2[0].rating) {
+		console.log("player 2 card " + a2[0].rating + " wins over: " + a1[0].rating);
+		p2[0] = a1.shift();
+		p2[1] = a2.shift();
+		// console.log("player 1 array length: " + player1PlayedCards.length);
+		// console.log("player 2 array length: " + player2PlayedCards.length);	
+		testForWin();
+	} else if(a1[0].rating === a2[0].rating) {
 			alert("It's a WAR!");
-			// gamePlayArray[cardCounter] = player1ActiveCards.shift();
-			// cardCounter++;
-			// gamePlayArray[cardCounter] = player2ActiveCards.shift();
-			// cardCounter++;
+			if(testForCards(a1, a2, p1, p2)) {
+				// playWar(); 
+			}
 	}
 
 }
